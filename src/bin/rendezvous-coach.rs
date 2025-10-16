1b90a960cfb0ad11ec9ce1bc3e61755ee1bf7d7a
Manuel Paccagnella <manuel.paccagnella@pm.me>
2025-10-16 11:38:57 +0200

    Extracted TTS into dedicated feature

--- src/bin/rendezvous-coach.rs
+++ src/bin/rendezvous-coach.rs
@@ -1,21 +1,32 @@
+use chrono::{Duration, prelude::*};
 use error_stack::ResultExt;
 use rendezvous_coach::error::{AppError, AppResult};
+use rendezvous_coach::feature::tts::{Speaker, TTSSpeaker};
 use rendezvous_coach::init;
-use tts::Tts;
+
+#[derive(Debug)]
+struct Plan {
+    rendezvous_time: DateTime<Local>,
+    trip_duration: Duration,
+    alerting_duration: Duration,
+}
 
 fn main() -> AppResult<()> {
     init::error_reporting();
     init::tracing();
 
-    let mut tts = Tts::default()
-        .change_context(AppError)
-        .attach("cannot initialize TTS engine")?;
-    let features = tts.supported_features();
-    println!("Features: {:?}", features);
+    let mut speaker = TTSSpeaker::new().change_context(AppError)?;
+    speaker.speak("Oggi è una bella giornata!").change_context(AppError)?;
+    speaker.speak("Mancano 15 minuti alla partenza").change_context(AppError)?;
 
-    tts.speak("Oggi è una bella giornata!", true)
-        .change_context(AppError)
-        .attach("cannot speak")?;
+    //std::thread::sleep(Duration::seconds(1).to_std().unwrap());
+
+    let plan = Plan {
+        rendezvous_time: Local::now(),
+        trip_duration: Duration::minutes(15),
+        alerting_duration: Duration::minutes(1 * 60 + 30),
+    };
+    dbg!(&plan);
 
     Ok(())
 }
---
+++ src/feature/tts/mod.rs
@@ -0,0 +1,40 @@
+use error_stack::{Report, ResultExt};
+use tracing::info;
+use tts::Tts;
+
+#[derive(Debug, thiserror::Error)]
+#[error("TTS error")]
+pub struct SpeakerError;
+
+type SpeakerResult<T> = Result<T, Report<SpeakerError>>;
+
+pub trait Speaker {
+    fn speak(&mut self, content: &str) -> SpeakerResult<()>;
+}
+
+pub struct TTSSpeaker {
+    tts: Tts,
+}
+
+impl TTSSpeaker {
+    pub fn new() -> SpeakerResult<TTSSpeaker> {
+        let tts = Tts::default()
+            .change_context(SpeakerError)
+            .attach("cannot initialize TTS engine")?;
+
+        let features = tts.supported_features();
+        info!("TTS features: {:?}", features);
+
+        Ok(TTSSpeaker { tts })
+    }
+}
+
+impl Speaker for TTSSpeaker {
+    fn speak(&mut self, content: &str) -> SpeakerResult<()> {
+        self.tts
+            .speak(content, true)
+            .change_context(SpeakerError)
+            .attach("cannot speak")?;
+        Ok(())
+    }
+}
